41b8e9729143787629055d3ad2b9abfd043b7194
griffinschoo <azizi-family@hotmail.com>
2024-05-09 17:40:30 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+function changecolour(){
+    document.getElementById("heading1").style.color = "red";
+    document.getElementById("body").style.backgroundColor = "skyblue";
+    document.getElementById("button").style.display = "none";
+}
